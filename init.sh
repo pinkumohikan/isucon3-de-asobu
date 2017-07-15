7d31a70ada2d398cc626cb45ccfc0e5a0ce8582a
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2017-07-15 09:35:47 +0000

    fix(init): index貼るとか

--- init.sh
+++ init.sh
@@ -4,10 +4,9 @@ date > /home/isucon/bench-kun.log
 
 echo 'alter table memos add column username varchar(255) null;' | mysql -u isucon isucon
 echo 'alter table users add index username(`username`);' | mysql -u isucon isucon
+echo 'alter table memos add index user(`user`);' | mysql -u isucon isucon
 echo 'update memos as m, users as u set m.username = u.username where m.user = u.id;' | mysql -u isucon isucon
-echo 'alter table memos add index is_private_and_created_at(`is_private`, `created_at`);' | mysql -u isucon isucon
 
-echo 'alter table memos;' | mysql -u isucon isucon
-echo 'alter table users;' | mysql -u isucon isucon
+echo 'alter table memos add index is_private_and_created_at(`is_private`, `created_at`);' | mysql -u isucon isucon
 
 echo 'initialized!' >> /home/isucon/bench-kun.log
